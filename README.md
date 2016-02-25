4b95ab10b9314e474f6a4a23f31adcc658483f5c
Daniel Guija <dani.guija@i2cat.net>
2016-02-25 16:05:50 +0000

    First version of sdk-ns-catalogue API, featuring GET and POST methods

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# son-catalogue
-SONATA's Catalogues
+# SONATA WP3/WP4
+
+## SDK/SP NS Catalogue
+
+### Requirements
+
+This code has been run on Ruby 2.1.
+
+### Gems used
+
+* [Sinatra](http://www.sinatrarb.com/) - Ruby framework
+* [Thin](https://github.com/macournoyer/thin/) - Web server
+* [json](https://github.com/flori/json) - JSON specification
+* [sinatra-contrib](https://github.com/sinatra/sinatra-contrib) - Sinatra extensions
+* [Nokogiri](https://github.com/sparklemotion/nokogiri) - XML parser
+* [JSON-schema](https://github.com/ruby-json-schema/json-schema) - JSON schema validator
+* [Rest-client](https://github.com/rest-client/rest-client) - HTTP and REST client
+* [Yard](https://github.com/lsegal/yard) - Documentation generator tool
+* [rerun](https://github.com/alexch/rerun) - Restarts the app when a file changes (used in development environment)
+
+### Installation
+
+First, a fresh MongoDB installation is required, working on localhost:27017
+After cloning the source code from the repository, then you can run
+
+```sh
+bundle install
+```
+
+Which will install all the gems needed to run the SON-CATALOGUE API.
+
+### Tests
+
+TODO: Unit-tests, intengration-tests
+
+### API Documentation
+
+The API documentation is expected to be generated with APIDOC soon.
+Currently, the API is documented with yardoc and can be built with a rake task:
+
+```sh
+rake yard
+```
+
+from here you can use the yard server to browse the docs from the source root:
+
+```sh
+yard server
+```
+
+and they can be viewed from http://localhost:8808/
+
+### Run Server
+
+The following shows how to start the API server:
+
+```sh
+rake start
+```
---
+++ son-sdk-catalogue/sdk-ns-catalogue/helpers/ns.rb
@@ -0,0 +1,132 @@
+
+# @see NsCatalogue
+class SonataNsCatalogue < Sinatra::Application
+
+  require 'json'
+  require 'yaml'
+
+	# Checks if a JSON message is valid
+	#
+	# @param [JSON] message some JSON message
+	# @return [Hash, nil] if the parsed message is a valid JSON
+	# @return [Hash, String] if the parsed message is an invalid JSON
+	def parse_json(message)
+		# Check JSON message format
+		begin
+			parsed_message = JSON.parse(message) # parse json message
+		rescue JSON::ParserError => e
+			# If JSON not valid, return with errors
+			logger.error "JSON parsing: #{e.to_s}"
+			return message, e.to_s + "\n"
+		end
+
+		return parsed_message, nil
+	end
+
+  # Checks if a YAML message is valid
+  #
+  # @param [YAML] message some YAML message
+  # @return [Hash, nil] if the parsed message is a valid YAML
+  # @return [Hash, String] if the parsed message is an invalid YAML
+  def parse_yaml(message)
+    # Check YAML message format
+    begin
+      parsed_message = YAML.load(message) # parse YAML message
+      #puts 'PARSED_MESSAGE: ', parsed_message.to_yaml
+    rescue YAML::ParserError => e
+      # If YAML not valid, return with errors
+      logger.error "YAML parsing: #{e.to_s}"
+      return message, e.to_s + "\n"
+    end
+
+    return parsed_message, nil
+  end
+
+  # Translates a message from YAML to JSON
+  #
+  # @param [YAML] input_yml some YAML message
+  # @return [Hash, nil] if the input message is a valid YAML
+  # @return [Hash, String] if the input message is an invalid YAML
+	def yaml_to_json(input_yml)
+    #puts input_yml.to_s
+    puts 'Parsing from YAML to JSON'
+
+		begin
+			#output_json = JSON.dump(YAML::load(input_yml))
+      #puts 'input: ', input_yml.to_json
+      output_json = JSON.dump(input_yml)
+      #output_json = JSON.dump(input_yml.to_json)
+		rescue
+			logger.error "Error parsing from YAML to JSON"
+			end
+
+    puts 'Parsing DONE', output_json
+		return output_json
+	end
+
+  # Translates a message from JSON to YAML
+  #
+  # @param [JSON] input_json some JSON message
+  # @return [Hash, nil] if the input message is a valid JSON
+  # @return [Hash, String] if the input message is an invalid JSON
+	def json_to_yaml(input_json)
+		require 'json'
+		require 'yaml'
+
+		begin
+			output_yml = YAML.dump(JSON.parse(input_json))
+		rescue
+			logger.error "Error parsing from JSON to YAML"
+			end
+
+		return output_yml
+	end
+
+  # Builds an HTTP link for pagination
+	def build_http_link(offset, limit)
+		link = ''
+		# Next link
+		next_offset = offset + 1
+		next_nss = Ns.paginate(:page => next_offset, :limit => limit)
+		begin
+			link << '<localhost:4011/network-services?offset=' + next_offset.to_s + '&limit=' + limit.to_s + '>; rel="next"' unless next_nss.empty?
+		rescue
+			logger.error "Error Establishing a Database Connection"
+		end
+
+		unless offset == 1
+			# Previous link
+			previous_offset = offset - 1
+			previous_nss = Ns.paginate(:page => previous_offset, :limit => limit)
+			unless previous_nss.empty?
+				link << ', ' unless next_nss.empty?
+				link << '<localhost:4011/network-services?offset=' + previous_offset.to_s + '&limit=' + limit.to_s + '>; rel="last"'
+			end
+		end
+		link
+	end
+
+	def build_http_link_name(offset, limit, name)
+		link = ''
+		# Next link
+		next_offset = offset + 1
+		next_nss = Ns.paginate(:page => next_offset, :limit => limit)
+		begin
+			link << '<localhost:4011/network-services/name/' + name.to_s + '?offset=' + next_offset.to_s + '&limit=' + limit.to_s + '>; rel="next"' unless next_nss.empty?
+		rescue
+			logger.error "Error Establishing a Database Connection"
+		end
+
+		unless offset == 1
+			# Previous link
+			previous_offset = offset - 1
+			previous_nss = Ns.paginate(:page => previous_offset, :limit => limit)
+			unless previous_nss.empty?
+				link << ', ' unless next_nss.empty?
+				link << '<localhost:4011/network-services/name/' + name.to_s + '?offset=' + previous_offset.to_s + '&limit=' + limit.to_s + '>; rel="last"'
+			end
+		end
+		link
+	end
+	
+end
