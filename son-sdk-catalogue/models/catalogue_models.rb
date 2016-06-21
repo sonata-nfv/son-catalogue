e7f15a87a9b48247530627eec1eff703b0ab136e
Daniel Guija <dani.guija@i2cat.net>
2016-06-21 12:32:07 +0100

    Fixed mongo-express docker. Fixed some codestyles warnings

--- son-sdk-catalogue/models/catalogue_models.rb
+++ son-sdk-catalogue/models/catalogue_models.rb
@@ -1,46 +1,46 @@
 # Convert BSON ID to String
 module BSON
-	class ObjectId
-		def to_json(*args)
-			to_s.to_json
-		end
+  class ObjectId
+    def to_json(*)
+      to_s.to_json
+    end
 
-		def as_json(*args)
-			to_s.as_json
-		end
-	end
+    def as_json(*)
+      to_s.as_json
+    end
+  end
 end
 
 class Ns
-	include Mongoid::Document
-	include Mongoid::Timestamps
-	include Mongoid::Pagination
-	include Mongoid::Attributes::Dynamic
-	store_in session: "ns_db"
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Pagination
+  include Mongoid::Attributes::Dynamic
+  store_in session: "ns_db"
 
-	#field :nsd, type: Hash
-	field :vendor, type: String
-	field :name, type: String
-	field :version, type: String
+  # field :nsd, type: Hash
+  field :vendor, type: String
+  field :name, type: String
+  field :version, type: String
 
 
-	validates :vendor, :name, :version, :presence => true
+  validates :vendor, :name, :version, :presence => true
 
 end
 
 class Vnf
-	include Mongoid::Document
-	include Mongoid::Timestamps
-	include Mongoid::Pagination
-	#include Mongoid::Versioning
-	include Mongoid::Attributes::Dynamic
-	store_in session: "vnf_db"
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Pagination
+  # include Mongoid::Versioning
+  include Mongoid::Attributes::Dynamic
+  store_in session: "vnf_db"
 
-	field :vendor, type: String
-	field :name, type: String
-	field :version, type: String
-	#field :vnf_manager, type: String # <- Not applicable yet
-	#field :vnfd, type: Hash
+  field :vendor, type: String
+  field :name, type: String
+  field :version, type: String
+  # field :vnf_manager, type: String # <- Not applicable yet
+  # field :vnfd, type: Hash
 
-	validates :vendor, :name, :version, :presence => true
+  validates :vendor, :name, :version, :presence => true
 end
