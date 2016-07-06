2733f4ebbae571630160125819c241c4ce198df6
Daniel Guija <dani.guija@i2cat.net>
2016-07-06 13:11:36 +0100

    Added license headers. Codestyle fixes

--- son-sdk-catalogue/main.rb
+++ son-sdk-catalogue/main.rb
@@ -1,10 +1,24 @@
+##
+## Copyright 2015-2017 i2CAT Foundation
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##   http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
 # Set environment
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra'
 require 'sinatra/config_file'
 require 'yaml'
-#require 'logstash-logger'
 #require 'sinatra/gk_auth' # <- Disabled
 
 # Require the bundler gem and then call Bundler.require to load in all gems
@@ -20,7 +34,7 @@ configure do
 	# Configuration for logging
 	enable :logging
 	Dir.mkdir("#{settings.root}/log") unless File.exists?("#{settings.root}/log")
-	log_file = File.new("#{settings.root}/log/#{settings.environment}.log", "a+")
+	log_file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
 	log_file.sync = true
 	use Rack::CommonLogger, log_file
 end
@@ -34,5 +48,6 @@ class SonataCatalogue < Sinatra::Application
 	# Load configurations
 	config_file 'config/config.yml'
 	Mongoid.load!('config/mongoid.yml')
-	#use Rack::CommonLogger, LogStashLogger.new(host: settings.logstash_host, port: settings.logstash_port)
+	# use Rack::CommonLogger,
+  # LogStashLogger.new(host: settings.logstash_host, port: settings.logstash_port)
 end
